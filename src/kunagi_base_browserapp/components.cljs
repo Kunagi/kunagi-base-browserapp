e8ad2520a67fd8a268014e45358971958145c68a
Witoslaw Koczewski <wi@koczewski.de>
2019-09-16 22:03:56 +0200

    PermissionBoundary

--- src/kunagi_base_browserapp/components.cljs
+++ src/kunagi_base_browserapp/components.cljs
@@ -2,7 +2,23 @@
   (:require
    ["@material-ui/core" :as mui]
    ["@material-ui/icons" :as icons]
+   [mui-commons.components :as muic]
    [mui-commons.api :refer [<subscribe]]))
+
+
+(defn- user-has-permission? [req-perm]
+  (when-let [user (<subscribe [:auth/user])]
+    (let [user-perms (or (-> user :user/perms)
+                         #{})]
+      (user-perms req-perm))))
+
+
+(defn PermissionBoundary [req-perm protected-component alternative-component]
+  [:div.PermsBoundary
+   (if (user-has-permission? req-perm)
+     protected-component
+     alternative-component)])
+
 
 (defn ConnectionStatusIndicator []
   (let [state (<subscribe [:http-async/state])
@@ -11,10 +27,12 @@
       [:> mui/IconButton
        {:disabled true
         :title "Online"
+        :size :small
         :style {:color :grey}}
        [:> icons/Link]]
       [:> mui/IconButton
        {:disabled true
         :title "Offline"
+        :size :small
         :style {:color :red}}
        [:> icons/LinkOff]])))

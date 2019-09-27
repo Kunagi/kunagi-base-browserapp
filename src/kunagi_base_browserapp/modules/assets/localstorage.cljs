b7026f1e9e41fe2c7861845eca35b192eaea6143
Witoslaw Koczewski <wi@koczewski.de>
2019-09-27 18:52:21 +0200

    store assets in localstorage

--- src/kunagi_base_browserapp/modules/assets/localstorage.cljs
+++ src/kunagi_base_browserapp/modules/assets/localstorage.cljs
@@ -1,9 +1,43 @@
-(ns kunagi-base-browserapp.modules.assets.localstorage)
+(ns kunagi-base-browserapp.modules.assets.localstorage
+  (:require
+   [cljs.reader :refer [read-string]]))
 
 
-(defn load-asset [asset asset-path]
-  nil)
+(defn set-item!
+  "Set `key' in browser's localStorage to `val`."
+  [key val]
+  (.setItem (.-localStorage js/window) key val))
+
+(defn get-item
+  "Returns value of `key' from browser's localStorage."
+  [key]
+  (.getItem (.-localStorage js/window) key))
+
+(defn remove-item!
+  "Remove the browser's localStorage value for the given `key`"
+  [key]
+  (.removeItem (.-localStorage js/window) key))
 
 
-(defn on-asset-updated [asset asset-path value]
-  (tap> [:!!! ::asset-updated asset asset-path value]))
+(defn storage-key [asset-pool asset-path]
+  (let [asset-pool-ident (-> asset-pool :asset-pool/ident)]
+    (str "assets/asset "
+         (namespace asset-pool-ident)
+         " "
+         (name asset-pool-ident)
+         " "
+         (-> asset-path))))
+
+
+(defn load-asset [asset-pool asset-path]
+  (when (-> asset-pool :asset-pool/localstorage?)
+    (tap> [:!!! ::load-asset (storage-key asset-pool asset-path)])
+    (when-let [s (get-item (storage-key asset-pool asset-path))]
+      (tap> [:!!! ::load-asset s])
+      (read-string s))))
+
+
+(defn on-asset-updated [asset-pool asset-path value]
+  (when (-> asset-pool :asset-pool/localstorage?)
+    (tap> [:!!! ::storing asset-pool asset-path value])
+    (set-item! (storage-key asset-pool asset-path) (pr-str value))))

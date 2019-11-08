3f66e25cff104d2228c17cf17f7de23e2f6ab3e7
Witoslaw Koczewski <wi@koczewski.de>
2019-11-08 08:58:24 +0100

    devtools: tap

--- src/kunagi_base_browserapp/modules/devtools/model.cljs
+++ src/kunagi_base_browserapp/modules/devtools/model.cljs
@@ -2,15 +2,31 @@
   (:require
    [kunagi-base.appmodel :refer [def-module]]
    [kunagi-base.modules.startup.model :refer [def-init-function]]
+   [kunagi-base-browserapp.modules.desktop.model :refer [def-page]]
 
-   [kunagi-base-browserapp.modules.devtools.api :as impl]))
+   [kunagi-base-browserapp.modules.devtools.api :as impl]
+   [kunagi-base-browserapp.modules.devtools.tap :as tap]))
 
 
 (def-module
   {:module/id ::devtools})
 
 
+(def-page
+  {:page/id ::tap-page
+   :page/ident :devtools-tap
+   :page/module [:module/ident :devtools]
+   :page/title-text "devtools: tap>"
+   :page/workarea [tap/Workarea]})
+
+
 (def-init-function
   {:init-function/id ::graphed
    :init-function/module [:module/ident :devtools]
    :init-function/f #(impl/init-graphed %)})
+
+
+(def-init-function
+  {:init-function/id ::tap
+   :init-function/module [:module/ident :devtools]
+   :init-function/f #(tap/init-tap! %)})
---
+++ src/kunagi_base_browserapp/modules/devtools/tap.cljs
@@ -0,0 +1,37 @@
+(ns kunagi-base-browserapp.modules.devtools.tap
+  (:require
+   [reagent.core :as r]
+   [kunagi-base.logging.tap :as logging-tap]
+   [mui-commons.components :as muic]))
+
+
+(defonce !records (r/atom '()))
+
+
+(def card-colors
+  {:err "#ff7043"
+   :wrn "#ffa726"
+   :inf "#d4e157"})
+
+(defn Record [record]
+  [muic/Card
+   {:style {:background-color (-> record :level card-colors)}}
+   [muic/Data record]])
+
+
+(defn Records []
+
+  [muic/Column
+   {:items (take 10 @!records)
+    :template [Record]}])
+
+
+(defn Workarea []
+  [Records])
+
+
+(defn init-tap! [db]
+  (add-tap #(swap! !records conj (logging-tap/o->record %)))
+  db)
+
+
